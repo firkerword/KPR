b82d22ced13264a900f5f220365e856c109f19f9
firkerword <61251923+firkerword@users.noreply.github.com>
2021-02-05 21:19:11 +0800

    Create jd.sh

---
+++ jd.sh
@@ -0,0 +1,14 @@
+
+# ------------------- 京东签到一键脚本！ ------------------- 
+
+opkg update
+
+opkg install git git-http node node-npm
+
+npm install -g crypto-js got http-server tough-cookie download request tunnel qrcode-terminal
+
+git clone -b main https://github.com/firkerword/JD_Script.git /usr/share/JD_Script
+
+chmod 777 /usr/share/JD_Script/jd.sh && sh /usr/share/JD_Script/jd.sh
+
+sh $jd update
