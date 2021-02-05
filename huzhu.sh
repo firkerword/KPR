d22de5deee11271879085f2b91edfbf03c44671f
firkerword <61251923+firkerword@users.noreply.github.com>
2021-02-05 22:36:57 +0800

    Add files via upload

---
+++ huzhu.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+#
+	#京东农场
+        new_fruit="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/6632c8135/&$new_fruit/" /usr/share/JD_Script/jd.sh
+
+	#萌宠
+        new_pet="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/1OTQ4Njk==/&$new_pet/" /usr/share/JD_Script/jd.sh
+
+	#种豆
+        new_plantBean="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/vj7hcdr5a/&$new_plantBean/" /usr/share/JD_Script/jd.sh
+
+	#京喜工厂
+        new_dreamFactory="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/QbQTfFg==/&$new_dreamFactory/" /usr/share/JD_Script/jd.sh
+
+	#东东工厂
+        new_jdfactory="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/Dd8CqABxo/&$new_jdfactory/" /usr/share/JD_Script/jd.sh
+
+	#京东赚赚长期活动
+        new_jdzz="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/8j38cwA/&$new_jdzz/" /usr/share/JD_Script/jd.sh
+
+	#crazyJoy任务
+        new_crazyJoy="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/9zd5YaBeE/&$new_crazyJoy/" /usr/share/JD_Script/jd.sh
+
+	#口袋书店
+        new_jdbook="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/d496591d0/&$new_jdbook/" /usr/share/JD_Script/jd.sh
+
+	#京喜农场
+        new_jxnc="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/f469634395/&$new_jxnc/" /usr/share/JD_Script/jd.sh
+
+	#签到领现金
+        new_jdcash="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/eU9Ya-iyZ68kpWrRmXBFgw/&$new_jdcash/" /usr/share/JD_Script/jd.sh
+
+	#爱消除
+        new_jdgyec="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/743359/&$new_jdgyec/" /usr/share/JD_Script/jd.sh
+
+	#京东炸年兽
+        new_jdnian="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/Vxvi1jGQt4/&$new_jdnian/" /usr/share/JD_Script/jd.sh
+
+	#京东神仙书院
+        new_jdimmortal="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/9wUBTjm2pNOZkO1C8ew/&$new_jdimmortal/" /usr/share/JD_Script/jd.sh
+
+	#闪购盲盒
+        new_jdsgmh="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/RcIQCjVWmIaW5kRrbA/&$new_jdsgmh/" /usr/share/JD_Script/jd.sh
+
+	#京东手机年终奖
+        new_jdfestival="互助码填写位置"
+        #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
+        sed -i "s/af03-6a4cd00f94ec/&$new_jdfestival/" /usr/share/JD_Script/jd.sh
+
+
+
