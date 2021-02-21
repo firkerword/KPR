dd0d07a39da723e093b362fa56db82971a081f62
firkerword <61251923+firkerword@users.noreply.github.com>
2021-02-21 19:12:08 +0800

    Update jdup.sh

--- jdup.sh
+++ jdup.sh
@@ -3,7 +3,7 @@
 
 opkg update
 
-opkg install git git-http node node-npm
+opkg install git git-http node node-npm openssh-client openssh-keygen
 
 npm install -g crypto-js got http-server tough-cookie download request tunnel qrcode-terminal websocket
 
