6dce43d8ea57421388808fd4d237c8db322b6d1f
firkerword <61251923+firkerword@users.noreply.github.com>
2021-03-04 12:16:58 +0800

    Update jdup.sh

--- jdup.sh
+++ jdup.sh
@@ -7,7 +7,7 @@ opkg install git git-http node node-npm openssh-client openssh-keygen python3
 
 npm install -g crypto-js got http-server tough-cookie download request tunnel qrcode-terminal websocket
 
-git clone -b main https://github.com/firkerword/JD_Script.git /usr/share/JD_Script
+git clone -b main https://github.com/firkerword/JD_Script.git /usr/share/Install_script/JD_Script
 
 chmod 777 /usr/share/JD_Script/jd.sh
 
