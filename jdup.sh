77ef5e93ade3a5d73744ad5a4f5fd8f47045bd9e
firkerword <61251923+firkerword@users.noreply.github.com>
2021-03-17 19:23:37 +0800

    Update jdup.sh

--- jdup.sh
+++ jdup.sh
@@ -7,11 +7,11 @@ opkg install libnghttp214 git git-http node node-npm openssh-client openssh-keyg
 
 npm install -g crypto-js got http-server tough-cookie download request tunnel qrcode-terminal websocket
 
-git clone -b main https://github.com/firkerword/JD_Script.git /usr/share/JD_Script
+git clone -b main https://github.com/firkerword/JD_Script.git /usr/share/jd_openwrt_script/JD_Script
 
-chmod 777 /usr/share/JD_Script/jd.sh
+chmod 777 /usr/share/jd_openwrt_script/JD_Script/jd.sh
 
-cd /usr/share/JD_Script && sh jd.sh 
+cd /usr/share/jd_openwrt_script/JD_Script && sh jd.sh 
 
 # sh $jd npm_install
 
