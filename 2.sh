a142d3cda230bd62ff8188db1de293df81a72dd4
firkerword <61251923+firkerword@users.noreply.github.com>
2021-04-03 14:25:09 +0800

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -6,7 +6,7 @@ rm -rf ./JDScript/README.md
 cp -r ./JDScript/* ./JD_Script
 
 	#京东农场
-        new_fruit="0763443f7d6f4f5ea5e54adc1c6112ed@e61135aa1963447fa136f293a9d161c1@f9e6a916ad634475b8e77a7704b5c3d8@"
+        new_fruit="0763443f7d6f4f5ea5e54adc1c6112ed@e61135aa1963447fa136f293a9d161c1@f9e6a916ad634475b8e77a7704b5c3d8@713f6d7bd3084b4eb157375e119ef521@"
         #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
         sed -i "s/6632c8135/$new_fruit&/" JD_Script/jd.sh
 
@@ -21,7 +21,7 @@ cp -r ./JDScript/* ./JD_Script
         sed -i "s/4npkonns/$new_plantBean&/" JD_Script/jd.sh
 
 	#京喜工厂
-        new_dreamFactory="X2poJVLcLoygZX0TgGmkl8EiBIkQe_zrMAZqtgL24-M=@5MIEocu93aHBEq_1DLOFFA==@"
+        new_dreamFactory="X2poJVLcLoygZX0TgGmkl8EiBIkQe_zrMAZqtgL24-M=@5MIEocu93aHBEq_1DLOFFA==@DAJklWSqv4HcjA35zvIPGA==@"
         #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
         sed -i "s/4HL35B/$new_dreamFactory&/" JD_Script/jd.sh
 
@@ -31,7 +31,7 @@ cp -r ./JDScript/* ./JD_Script
         sed -i "s/P04z54XCjVWnYaS5m9cZ2f83X0Zl/$new_jdfactory&/" JD_Script/jd.sh
 
 	#京东赚赚长期活动
-        new_jdzz="95OquUc_sFugJO5_E_2dAgm-@eU9YELv7P4thhw6utCVw@eU9YaOjnbvx1-Djdz3UUgw@"
+        new_jdzz="95OquUc_sFugJO5_E_2dAgm-@eU9YELv7P4thhw6utCVw@eU9YaOjnbvx1-Djdz3UUgw@S5KkcRx0b81fRIkigwKZccQ@"
         #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
         sed -i "s/AUWE5mKm/$new_jdzz&/" JD_Script/jd.sh
 
@@ -51,7 +51,7 @@ cp -r ./JDScript/* ./JD_Script
         sed -i "s/019cffd91086/$new_jxnc&/" JD_Script/jd.sh
 	
 	#签到领现金
-        new_jdcash="95OquUc_sFugJO5_E_2dAgm-@eU9YELv7P4thhw6utCVw@eU9YaOjnbvx1-Djdz3UUgw@eU9Yau_gMP4nombWynsU1w@eU9YaOWxZPV09zrQwnQX0w@"
+        new_jdcash="95OquUc_sFugJO5_E_2dAgm-@eU9YELv7P4thhw6utCVw@eU9YaOjnbvx1-Djdz3UUgw@eU9Yau_gMP4nombWynsU1w@eU9Ya--wYP8g8zyBnyJF0w@"
         #互助码填写格式：@+互助码    例如：@sd452a41ds2af1a1
         sed -i "s/eU9Ya-iyZ68kp/$new_jdcash&/" JD_Script/jd.sh
 	
