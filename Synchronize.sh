34ff2f4ee5f5db4bd57c50f62cc071361aa48db1
firkerword <61251923+firkerword@users.noreply.github.com>
2022-03-08 17:51:16 +0800

    Update Synchronize.sh

--- Synchronize.sh
+++ Synchronize.sh
@@ -2,6 +2,8 @@
 #
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/smartdns
 
+rm -rf ./smartdns/.svn
+
 git clone https://github.com/firker/diy-ziyong.git
 
 cp -r ./smartdns ./diy-ziyong
